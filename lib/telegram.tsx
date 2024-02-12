f653877e8d43923c7c2a1c6bab32991181ce0a3a
davronkhandamov <xamdamovdavron6@gmail.com>
2024-02-12 19:50:45 +0500

    added telegram api

--- lib/telegram.tsx
+++ lib/telegram.tsx
@@ -1,8 +1,8 @@
 export function sendToTelegram(message: string) {
-    fetch("https://api.telegram.org/bot" + process.env.NEXT_APP_TELEGRAM_BOT_TOKEN + "/sendMessage", {
+    fetch("https://api.telegram.org/bot1776989865:AAE44uMgmtGYhmSWcZy165cPfVOs6-7O_54/sendMessage", {
         method: "POST",
         body: JSON.stringify({
-            chat_id: process.env.NEXT_APP_CHAT_ID,
+            chat_id: "836696307",
             text: message,
             'parse_mode': 'Markdown'
         }),
