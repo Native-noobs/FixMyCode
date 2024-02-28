6aaf3233adacc0162bd2c48b328625e8b1db26b5
Davron <109721370+davronkhamdamov@users.noreply.github.com>
2024-02-28 15:31:24 +0500

    Update order by

--- pages/api/lesson.tsx
+++ pages/api/lesson.tsx
@@ -12,7 +12,7 @@ export default async function handler(
                 homework: true
             },
             orderBy: {
-                createdAt: "desc"
+                createdAt: "asc"
             }
         })
         res.status(200).json({
